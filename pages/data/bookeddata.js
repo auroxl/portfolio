5c679d3007eb9a01a51206f7b0cc216dec252630
auroxl <72815032+auroxl@users.noreply.github.com>
2023-05-15 17:59:33 -0500

    Add files via upload

---
+++ pages/data/bookeddata.js
@@ -0,0 +1,30 @@
+const bookeddata = [
+    {   
+        id: 1,
+        image: 'https://dev.jlgriffin.me/booked1.jpg',
+        alt: "Booked Financial",
+    },
+    {
+        id: 2,
+        image: 'https://dev.jlgriffin.me/booked2.jpg',
+        alt: "Booked Financial",
+    },
+    {
+        id: 3,
+        image: 'https://dev.jlgriffin.me/booked3.jpg',
+        alt: "Booked Financial",
+    },
+    {
+        id: 4,
+        image: 'https://dev.jlgriffin.me/booked4.jpg',
+        alt: "Booked Financial",
+    },
+    {
+        id: 5,
+        image: 'https://dev.jlgriffin.me/booked5.jpg',
+        alt: "Booked Financial",
+    },
+];
+
+
+export default bookeddata;
